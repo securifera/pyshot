981049642d3db0b2b1a17a7ea22e7dffbf5df16e
root <root@kali>
2020-10-01 20:09:42 -0400

    Error checking

--- pyshot.py
+++ pyshot.py
@@ -286,25 +286,41 @@ def take_screenshot( host, port_arg, query_arg="", dest_dir="", secure=False, po
     #Remove characters that will make save fail
     filename += url.replace('://', '_').replace(':',"_")
 
-    if domain == None:
-
-        #Cleanup filename and save
-        filename1 = dest_dir + filename + ".png"
-        domain = chrome_screenshot(url, host, filename1)
+    #Cleanup filename and save
+    filename1 = dest_dir + filename + ".png"
+    ret_host = chrome_screenshot(url, host, filename1)
 
     #If the SSL certificate references a different hostname
-    if domain:
+    if domain and domain != ret_host:
 
         #Replace any wildcards in the certificate
         domain = domain.replace("*.", "")
         url = "https://" + host + ":443"
         
         #Add domain 
+        tmp_str = filename
         if domain != host:
-            filename += "_" + domain
-        filename2 = dest_dir + filename + ".png"
+            tmp_str += "_" + domain
+        filename2 = dest_dir + tmp_str + ".png"
         
         ret = phantomjs_screenshot(url, domain, filename2)
+
+    #If the SSL certificate references a different hostname
+    if ret_host:
+
+        #Replace any wildcards in the certificate
+        ret_host = ret_host.replace("*.", "")
+        url = "https://" + host + ":443"
+        
+        #Add domain
+        tmp_str = filename
+        if ret_host != host:
+            tmp_str += "_" + ret_host
+        filename2 = dest_dir + tmp_str + ".png"
+        
+        ret = phantomjs_screenshot(url, ret_host, filename2)
+
+
         if ret == True and filename1 and filename2:
             file_match = filecmp.cmp(filename1,filename2)
             if file_match:
